59aa230c386cb127ad80b8b9cd4f98a15bd4f703
Renee <reneegithinji@gmail.com>
2025-11-01 20:28:57 +0300

    Auto-commit Functions repo \2025-11-01 20:18:03

---
+++ auto-push-all.ps1
@@ -0,0 +1,13 @@
+# Auto-commit & push Manjano App repo
+cd "D:\Manjano\Projects\Manjano-Android"
+git add .
+\2025-11-01 20:18:03 = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+git commit -m "Auto-commit App repo \2025-11-01 20:18:03"
+git push origin main
+
+# Auto-commit & push Manjano Functions repo
+cd "D:\manjano-functions"
+git add .
+\2025-11-01 20:18:03 = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+git commit -m "Auto-commit Functions repo \2025-11-01 20:18:03"
+git push origin main
